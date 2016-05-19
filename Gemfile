4dd16d5646d725acbdcbf4c6ebc6c2709809cdd0
mouazq <mouazq@gmail.com>
2016-05-19 13:02:40 +0200

    Bootstrap init

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.5'
 gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'autoprefixer-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -20,6 +22,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create!(title: 'grocery shopping', content: 'pickles, eggs, red onion')
+Article.create!(title: 'wash the car')
+Article.create!(title: 'register kids for school', content: 'Register Kira for Ruby Junior High and Caleb for Rails High School')
+Article.create!(title: 'check engine light', content: 'The check engine light is on in the Tacoma')
+Article.create!(title: 'dog groomers', content: 'Take Pinky and Redford to the groomers on Wednesday the 23rd')
